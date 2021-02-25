457ebae132fffd86ee94a0d988bb88352bc10cb9
Eniola <69263854+Ayodeji10@users.noreply.github.com>
2021-02-25 02:15:39 +0100

    Added all files

---
+++ owl_carousel/owl-setup.js
@@ -0,0 +1,30 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 10,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        960: {
+            items: 5
+        },
+        1200: {
+            items: 11   // change this number to change number of items to appear
+        }
+    }
+});
+
+// delete this to stop the scroller effect
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY > 0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
